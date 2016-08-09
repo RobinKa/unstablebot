db18f8af9bf50355adc4fa5a804d03d9d953f30d
ToraxXx <xtremegosugaming@gmail.com>
2016-08-09 19:36:30 +0200

    added automatic guild news checking, added requirements.txt

--- bnet.py
+++ bnet.py
@@ -1,4 +1,5 @@
 from battlenet.community.wow.characters import Character
+from battlenet.community.wow.guilds import Guild
 from battlenet.oauth2 import BattleNetOAuth2
 from settings import *
 import re
@@ -42,6 +43,7 @@ def get_info(name, realm=BNET_REALM, fields=[]):
 	char = Character(apikey=BNET_KEY, realm=realm, name=name, fields=fields)
 	status_code, data = char.get()
 
+	print("Status code:", status_code)
 	print("Data:", data)
 
 	infos = []
@@ -51,7 +53,7 @@ def get_info(name, realm=BNET_REALM, fields=[]):
 	cl = CLASSES[int(data["class"])]
 	race = RACES[int(data["race"])]
 
-	info = "**Character** *Realm*: `%s` *Name*: `%s` *Class*: `%s` *Race*: `%s`" % (realm, name, cl, race)
+	info = "**Character** Realm: `%s` Name: `%s` Class: `%s` Race: `%s`" % (realm, name, cl, race)
 	infos.append(info)
 
 	def humanize_string(s):
@@ -61,7 +63,30 @@ def get_info(name, realm=BNET_REALM, fields=[]):
 		info = "**%s**" % field.title()
 		for k, v in data[field].items():
 			if not (isinstance(v, list) or isinstance(v, dict)):
-				info += " *%s*: `%s`" % (humanize_string(str(k)), humanize_string(str(v)))
+				info += " %s: `%s`" % (humanize_string(str(k)), humanize_string(str(v)))
 		infos.append(info)
 
 	return infos
+
+def get_guild_news(since_time, name, realm=BNET_REALM):
+	guild = Guild(apikey=BNET_KEY, name=name, realm=realm, fields=["news"])
+	status_code, data = guild.get()
+
+	print("GetGuildNews", since_time, "status:", status_code)
+
+	news = []
+	newest_time = since_time
+
+	for n in data["news"]:
+		news_time = int(n["timestamp"]) // 1000
+
+		if news_time > since_time:
+			news_type = n["type"]
+
+			if news_type == "playerAchievement":
+				news.append("*`%s` gained achievement `%s`*" % (n["character"], n["achievement"]["title"]))
+
+		if news_time > newest_time:
+			newest_time = news_time
+
+	return news, newest_time
--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 import discord as dc
 from settings import *
 import bnet
+import asyncio
+import datetime
 
 class UnstableClient(dc.Client):
+    def __init__(self):
+        super().__init__()
+        self.loop.create_task(self.check_guild_feed())
+
     async def on_ready(self):
         print("Ready")
         print("Name:", self.user.name)
@@ -44,6 +50,23 @@ class UnstableClient(dc.Client):
         else:
             char_name = s
             return char_name, BNET_REALM
+
+    async def check_guild_feed(self):
+        await self.wait_until_ready()
+
+        last_check_time = int(datetime.datetime.utcnow().timestamp())
+        print("Start check news time:", last_check_time)
+
+        channel = dc.Object(id=BOT_NEWS_CHECK_CHANNEL)
+
+        while not self.is_closed:
+            print("Checking news", last_check_time)
+            guild_news, last_check_time = bnet.get_guild_news(since_time=last_check_time, name=BOT_NEWS_CHECK_GUILD, realm=BOT_NEWS_CHECK_REALM)
+            print("Got news", last_check_time, guild_news)
+            for news in guild_news:
+                await self.send_message(channel, news)
+
+            await asyncio.sleep(30)
 
 client = UnstableClient()
 client.run(DISCORD_TOKEN)
